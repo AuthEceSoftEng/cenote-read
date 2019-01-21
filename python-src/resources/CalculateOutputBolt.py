69687fc21f88ba87c89b06dfc304f612c20fad6f
Napoleon Oikonomou <Napoleonoikon@gmail.com>
2019-01-21 15:43:10 +0200

    Add /extraction functionality

--- python-src/resources/CalculateOutputBolt.py
+++ python-src/resources/CalculateOutputBolt.py
@@ -2,17 +2,24 @@ import json
 
 import storm
 from CassandraHandler.utils.CassandraHandler import CassandraHandler
+from CassandraHandler.DataRead import ReadData as rd
 
 
 class CalculateOutputBolt(storm.BasicBolt):
   # Initialize this instance
+  def __init__(self):
+    self._context = None
+    self._conf = None
+    self.reader = None
+
   def initialize(self, conf, context):
-    self._conf = conf
     self._context = context
-    self.cql = CassandraHandler().execute_query
+    self._conf = conf
+    self.reader = rd()
 
   def process(self, tup):
     # ********** Read input arguments **********
+
     try:
       input_args = json.loads(tup.values[0])
     except:
@@ -20,14 +27,28 @@ class CalculateOutputBolt(storm.BasicBolt):
     ret_info = tup.values[1]
 
     # ********** Calculate results **********
-    answer = 42
-    if ("cql" in input_args):
-      if input_args["cql"]["mode"].lower() == "select":
-        answer = list(self.cql(
-            'select ' + input_args["cql"]["columns"] + ' from cenote.' + input_args["cql"]["table"])[0].items())
-
-    # ********** Return results **********
-    storm.emit([json.dumps(answer), ret_info])
+    if "query_type" not in input_args:
+      return storm.emit([json.dumps({"ok": False, "msg": 'No `query_type` param provided!'}), ret_info])
+    query_type = input_args["query_type"].lower()
+    if query_type == 'extraction':
+      timeframe_start = ''
+      timeframe_end = ''
+      if "timeframe_start" in input_args: timeframe_start = input_args["timeframe_start"]
+      if "timeframe_end" in input_args: timeframe_end = input_args["timeframe_end"]
+      info = {
+        "cenote": {
+          "url": "/projects/" + input_args["PROJECT_ID"] + "/queries/" + input_args["event_collection"] + "/extraction",
+          "timeframe_start": timeframe_start,
+          "timeframe_end": timeframe_end
+        }
+      }
+      if "target_property" in input_args:
+        answer = self.reader.read_data('cenote', input_args["target_property"].split(","), json.dumps(info))
+      else:
+        answer = self.reader.read_data('cenote', None, json.dumps(info))
+      return storm.emit([json.dumps({"ok": True, "msg": answer["data"]}), ret_info])
+    else:
+      return storm.emit([json.dumps({"ok": True, "msg": "Not implemented yet!"}), ret_info])
 
 # Start the bolt when it's invoked
 CalculateOutputBolt().run()
